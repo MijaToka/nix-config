330dbb4005dc6c41a748e95a66f4d4ccd3130128
Mijail Tokarev <mijail.tokarev@ug.uchile.cl>
2025-07-20 09:09:35 +0200

    Swaync config

---
+++ Mija/modules/swaync/default.nix
@@ -0,0 +1,49 @@
+{
+  services.swaync = {
+    enable = true;
+    settings = {
+      positionX = "right";
+      positionY = "top";
+      control-centre-radius = 1;
+      fit-to-screen = true;
+      layer-shell = true;
+      layer-overlay = true;
+      layer = "overlay";
+      control-center-layer = "overlay";
+      cssPriority = "user";
+      notification-icon-size = 64;
+      notification-body-image-height = 100;
+      notification-body-image-width = 200;
+      timeout = 10;
+      timeout-low = 5;
+      timeout-critical = 0;
+
+
+      widgets = [
+        "inhibitors"
+        "dnd"
+        "mpris"
+        "notifications"
+      ];
+      widgets-config = {
+        title = {
+          text = "notifications";
+	  clear-all-button = true;
+	  button-text = "Clear All";
+        };
+        dnd = {
+	  enabled = true;
+          text = "Do not Disturb";
+        };
+        mpris = {
+	  enabled = true;
+          image-size = 60;
+	  blur = true;
+        };
+	inhibitors = {
+          enabled = true;
+	};
+      };
+    };
+  };
+}
