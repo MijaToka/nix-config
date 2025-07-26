9226061124df3d18f31fdf1d727222e6154a301d
Mijail Tokarev <mijail.tokarev@ug.uchile.cl>
2025-07-26 18:36:42 +0200

    nvf config (buggy)

--- Mija/modules/nvf.nix
+++ Mija/modules/nvf.nix
@@ -1,5 +1,42 @@
-{
+{ config, lib, ... }:let
+  base16Scheme = config.stylix.base16Scheme;
+  base16-colors = lib.filterAttrs
+    (name: value: builtins.match "base0[0-9A-F]" name != null)
+    base16Scheme;
+in {
   programs.nvf = {
     enable = true;
+    settings = {
+      vim = {
+        theme = {
+          enable = true;
+          name = "base16";
+          inherit base16-colors;
+          style = "dark";
+          transparent = true;
+        };
+        languages = {
+          enableLSP = true;
+          enableTreesitter = true;
+
+          nix.enable = true;
+          python.enable = true;
+          markdown.enable = true;
+          clang.enable = true;
+        };
+
+        statusline.lualine = {
+          enable = true;
+          theme = "auto";
+        };
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
+      };
+    };
   };
 }
