8e3b2119df2b9ba5b34f90c8d30db21e6e27f966
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-17 16:47:02 +0200

    Tenatattive vscode configuration for hm (did not work, extensions were not being recognized)

---
+++ Mija/modules/vscode.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}:{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+    profiles = {
+      global = {
+        extensions = with pkgs.vscode-extensions; [
+          vscodevim.vim
+          # theqtcompany.qt-core
+          ms-python.python
+          ms-toolsai.jupyter
+          mkhl.direnv
+          arrterian.nix-env-selector
+          file-icons.file-icons
+          jnoortheen.nix-ide
+          tamasfe.even-better-toml
+        ];
+      };
+    };
+  };
+}
