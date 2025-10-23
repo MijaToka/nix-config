fffae0e557de1ef3e24b10800e4a52d59d785544
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-23 22:48:45 +0200

    Add lazy git

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,86 +1,94 @@
-{pkgs, ...}:{
-	nixpkgs.config = {
-		allowUnfree = true;
-	};
+{ pkgs, ... }:
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
 
-	programs = {
-		firefox.enable = true;
-		hyprland.enable = true;
-		direnv.enable = true;
-	};
+  programs = {
+    firefox.enable = true;
+    hyprland.enable = true;
+    direnv.enable = true;
+  };
 
-	environment.systemPackages = with pkgs; [
-		# CLI Utils
-		git # Version control
-		wget # 
-		file # Determine file type
+  environment.systemPackages = with pkgs; [
+    # CLI Utils
+    git # Version control
+    lazygit
+    wget
+    file # Determine file type
     fzf # Fuzzy finder
-		tree # Lists directories in tree structure
-		unzip # Compress and de-compress files
-		zip
-		ffmpeg # Convert audio and video sources
-		openssl
-		fastfetch # Display computer stats
-		neofetch
-		cava # TUI audio visualizer
-		xorg.xev # Device Input testing
-		nh # Nix commands
-		usbutils # USB commands
-		direnv # Environment management (for vscode)
-		vdhcoapp # CoApp to download videos from firefox
+    tree # Lists directories in tree structure
+    unzip # Compress and de-compress files
+    zip
+    ffmpeg # Convert audio and video sources
+    openssl
+    fastfetch # Display computer stats
+    neofetch
+    cava # TUI audio visualizer
+    xorg.xev # Device Input testing
+    nh # Nix commands
+    usbutils # USB commands
+    direnv # Environment management (for vscode)
+    vdhcoapp # CoApp to download videos from firefox
     zoxide # Better cd command
 
     # LaTeX
     (texlive.combine {
-		inherit (texlive) scheme-basic standalone varwidth scontents xcolor;
-	})
+      inherit (texlive)
+        scheme-basic
+        standalone
+        varwidth
+        scontents
+        xcolor
+        ;
+    })
 
-		# Desktop apps
-		audacity # Audio-mixing
-		rofi
-		wofi # App launcher
+    # Desktop apps
+    audacity # Audio-mixing
+    rofi
+    wofi # App launcher
 
-		# Editors
-		neovim # Text editor
-		xournalpp # Note taking app
-		rnote	
-		gimp3 # Image editor
+    # Editors
+    neovim # Text editor
+    xournalpp # Note taking app
+    rnote
+    gimp3 # Image editor
 
-		# Media player
-		mpv 	
-		vlc
+    # Media player
+    mpv
+    vlc
 
-		# Messaging apps
-		slack
-		mattermost
+    # Messaging apps
+    slack
+    mattermost
 
-		# DE and WM stuff
-		hyprland
-		hyprpaper
-		waybar
-		brightnessctl
-		mako
-		quickshell
+    # DE and WM stuff
+    hyprland
+    hyprpaper
+    waybar
+    brightnessctl
+    mako
+    quickshell
 
-		# Networkmanager GUI
-		# networkmanagerapplet
+    # Networkmanager GUI
+    # networkmanagerapplet
 
-		# Terminal emulators
-		kitty
+    # Terminal emulators
+    kitty
 
-		#Icons
-		adwaita-icon-theme
-		
-		# Wayland thingis
+    #Icons
+    adwaita-icon-theme
 
-		# Other
-		home-manager
+    # Wayland thingis
 
-		# Device drivers
-		sc-controller
-	];
-	
-	# fonts.packages = with pkgs; [
-	# 	nerdfonts
-	# ];
+    # Other
+    home-manager
+
+    # Device drivers
+    sc-controller
+  ];
+
+  # fonts.packages = with pkgs; [
+  # 	nerdfonts
+  # ];
 }
