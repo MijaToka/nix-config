ea3542dd5888d62b76b495f38847416ee7b02d01
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-23 22:43:53 +0200

    Boot loader configuration

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,4 +1,37 @@
+{ pkgs, ... }:
 {
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    # Boot loader
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+
+      # Hide generation choice
+      #(to choose, press a key during booting)
+      timeout = 0;
+    };
+
+    #Bootloader animation
+    plymouth = {
+      enable = true;
+      theme = "blockchain";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "blockchain" ];
+        })
+      ];
+    };
+
+    # Enable Silent Boot
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+
+  };
 }
