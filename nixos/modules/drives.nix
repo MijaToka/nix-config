ef9465ed9a64d628de797acde085d391b9c29911
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-23 22:43:37 +0200

    Fix auto mount

--- nixos/modules/drives.nix
+++ nixos/modules/drives.nix
@@ -1,6 +1,6 @@
 {
-  fileSystems."/run/media/mija/T7 Shield/" = {
-    device = "/dev/disk/by-uuid/37554bb2-0d9b-4760-ae7d-89dc56b0790a";
-    fsType = "ext4";
+  services = {
+    udisks2.enable = true;
+    dbus.enable = true;
   };
 }
