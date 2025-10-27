2e3cd3217ce2b0e334c3945b0ab89c061a8f21d2
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-27 17:16:15 +0100

    Display manager settings removed because of redundancy

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -1,26 +1,27 @@
-{ pkgs, ... }: {
-	programs.zsh.enable = true;
+{ pkgs, ... }:
+{
+  users = {
+    defaultUserShell = pkgs.zsh;
 
-	users = {
-		defaultUserShell = pkgs.zsh;
-
-		users.mija = {
-			isNormalUser = true;
-			description = "Mijail Tokarev";
-			extraGroups = [ "networkmanager" "wheel" "input" "libvirtd" ];
-			packages = with pkgs; [
-				vscode
-				discord
-				obsidian
-				zoom-us
-				spotify
-				whatsapp-for-linux
-				audacity
-			];
-		};
-	};
-
-	# Enable automatic login for the user.
-	services.displayManager.autoLogin.enable = false;
-	services.displayManager.autoLogin.user = "mija";
+    users.mija = {
+      isNormalUser = true;
+      description = "Mijail Tokarev";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "input"
+        "libvirtd"
+      ];
+      packages = with pkgs; [
+        vscode
+        discord
+        obsidian
+        zoom-us
+        spotify
+        whatsapp-for-linux
+        audacity
+      ];
+    };
+  };
 }
+
