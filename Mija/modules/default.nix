87ec19d83919ca626459120a1c0848f68bcbdc2b
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-27 18:15:44 +0100

    Add udiskie to HM

--- Mija/modules/default.nix
+++ Mija/modules/default.nix
@@ -7,11 +7,12 @@
     ./zsh
 
     ./fonts.nix
-    ./fzf.nix 
-    ./git.nix 
+    ./fzf.nix
+    ./git.nix
     ./kitty.nix
     # ./quickshell.nix
     ./stylix.nix
     # ./vscode.nix
+    ./udiskie.nix
   ];
 }
--- nixos/modules/udev.nix
+++ nixos/modules/udev.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:{
+{ pkgs, ... }:
+{
   services.udev = {
     enable = true;
     packages = with pkgs; [
       game-devices-udev-rules # Gaming controlers
-    ]; 
+    ];
     extraRules = ''
       #USB devices
       SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
@@ -20,11 +21,12 @@
       # Wired HORIPAD STEAM; USB
       KERNEL=="hidraw*", KERNELS=="*0F0D:01AB*", MODE="0660", TAG+="uaccess"
       KERNEL=="hidraw*", ATTRS{idVendor}=="0f0d", ATTRS{idProduct}=="01ab", MODE="0660", TAG+="uaccess"
-      
+
       # Wireless HORIPAD STEAM; Bluetooth
       KERNEL=="hidraw*", KERNELS=="*0F0D:0196*", MODE="0660", TAG+="uaccess"
       KERNEL=="hidraw*", ATTRS{idVendor}=="0f0d", ATTRS{idProduct}=="0196", MODE="0660", TAG+="uaccess"
-      '';
+    '';
   };
-    hardware.uinput.enable = true;
-  }
+  hardware.uinput.enable = true;
+}
+
