c15ab06b27da66498fd3bdddb9d8f7ad1bfd8574
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-10-31 11:18:44 +0100

    Formating

--- Mija/modules/default.nix
+++ Mija/modules/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ./hyprland
-    ./nvf
+    # ./nvf
     ./swaync
     ./wofi
     ./zsh
--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,26 +1,28 @@
-{pkgs, ...}:{
-	programs = {
-		steam = {
-			enable = true;
-			gamescopeSession.enable = true;
-			remotePlay.openFirewall = true;
-			dedicatedServer.openFirewall = true;
-		};
-		gamemode.enable = true;
+{ pkgs, ... }:
+{
+  programs = {
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+    gamemode.enable = true;
 
-	};
+  };
 
-	environment.sessionVariables = {
-		WLR_NO_HARDWARE_CURSORS = "1";
-	};
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
 
-	environment.systemPackages = with pkgs; [
-		# Gaming
-		lutris
-		winetricks
-		wineWowPackages.stable
-		wineWowPackages.waylandFull
-		mangohud
-		protonup
-	];
+  environment.systemPackages = with pkgs; [
+    # Gaming
+    lutris
+    winetricks
+    wineWowPackages.stable
+    wineWowPackages.waylandFull
+    mangohud
+    protonup-ng
+  ];
 }
+
