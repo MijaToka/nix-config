42725dccf7657e4df398b23c7f98e7f263bf166f
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-11-25 18:29:29 +0100

    Edits to osu, removing open tablet driver and correclty formatting the custom OTD systemd service

--- nixos/modules/custom-services.nix
+++ nixos/modules/custom-services.nix
@@ -15,20 +15,21 @@
         '';
       };
     };
-  };
 
-  rebind-wacom = {
-    description = "Rebind wacom driver from wacom tablet.";
-    wantedBy = [ ];
-    serviceConfig = {
-      type = "oneshot";
-      ExecStart = ''
-        for dev in /sys/bus/hid/devices/*; do 
-          if grep -qi "Vendor=056a" "$dev/uevent"; then
-            echo "$(basename $dev)" > /sys/bus/hid/drivers/wacom/bind || true 
-          fi 
-        done
-      '';
+    rebind-wacom = {
+      description = "Rebind wacom driver from wacom tablet.";
+      wantedBy = [ ];
+      serviceConfig = {
+        type = "oneshot";
+        ExecStart = ''
+          for dev in /sys/bus/hid/devices/*; do 
+            if grep -qi "Vendor=056a" "$dev/uevent"; then
+              echo "$(basename $dev)" > /sys/bus/hid/drivers/wacom/bind || true 
+            fi 
+          done
+        '';
+      };
     };
+
   };
 }
--- nixos/modules/wacom.nix
+++ nixos/modules/wacom.nix
@@ -1,8 +1,11 @@
+{ pkgs, lib, ... }:
 {
-  hardware.opentabletdriver = {
-    enable = true;
-    daemon.enable = false;
-  };
+  # services.opentabletdriver = {
+  #   enable = true;
+  #   daemon.enable = false;
+  # };
+  #
+  # systemd.services.opentabletdriver.wantedBy = lib.mkForce [ ];
 
   services.xserver.wacom.enable = true;
 
