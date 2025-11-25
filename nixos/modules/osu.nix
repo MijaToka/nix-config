14cfa2a6ecb61446da37e74c689cdc6292cc5de9
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-11-25 18:29:54 +0100

    Change osu-lazer to osu-lazer-bin (ie the online version)

--- nixos/modules/osu.nix
+++ nixos/modules/osu.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    osu-lazer
+    osu-lazer-bin
     (pkgs.writeShellScriptBin "osu-lazer-otd" ''
       systemctl start unbind-wacom.service
       systemctl start openTabletDriver.service
-      ${pkgs.osu.lazer}/bin/osu-lazer
+      ${pkgs.osu-lazer-bin}/bin/osu-lazer
       systemctl stop openTabletDriver.service
       systemctl start rebind-wacom.service
     '')
